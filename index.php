93d515bf77055cba1d2c7d582073f437da0a21ff
Łukasz <bazyl603@gmail.com>
2020-09-05 17:57:55 +0200

    stworzenie funkcji do debugowania i podstawowego html

--- index.php
+++ index.php
@@ -1,2 +1,44 @@
 <?php
-echo "hello word";
+
+declare(strict_types=1);
+
+namespace App;
+
+require_once("src/Utils/debug.php");
+
+$test = 'hola';
+
+dump($test);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Note</title>
+</head>
+<body>
+    <nav>
+        Navigation to creat
+    </nav>
+
+    <section>
+        <div class="menu">
+            <ul>
+                <li><a href="/">List of notes</a></li>
+                <li><a href="/?action=create">Create note</a></li>
+            </ul>
+        </div>
+
+        <div>
+            notes
+        </div>
+    </section>
+
+    <footer>
+        How create?
+    </footer>
+</body>
+</html>
