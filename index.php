fd65401b3215930607709f08c9430f141ce6e01d
Łukasz <bazyl603@gmail.com>
2020-09-07 21:52:46 +0200

    create controller class and add form to create not

--- index.php
+++ index.php
@@ -4,31 +4,15 @@ declare(strict_types=1);
 
 namespace App;
 
-//on in production
-//error_reporting(0);
-//ini_set('display_errors', '0');
+require_once("src/Utils/debug.php");
+require_once("src/Controller.php");
 
-require_once("src/Utils/debug.php");  //off in production
-require_once("src/View.php");
+$request = [
+  'get' => $_GET,
+  'post' => $_POST
+];
 
-const DEFAULT_ACTION = 'list';
+//$controller = new Controller($request);
+//$controller->run();
 
-// if (!empty($_GET['action'])){
-//     $action = $_GET['action'];
-// } else{
-//     $action = DEFAULT_ACTION;  |
-// } beter is bottom              \/
-$action = $_GET['action'] ?? DEFAULT_ACTION;
-
-$view = new View();
-
-$viewParams = [];
-if ($action === 'create'){
-    $page = 'createNote';
-    $viewParams['resultCreate'] = "success";
-} else{
-    $page = 'listNotes';
-    $viewParams['resultList'] = "display notes";
-}
-
-$view->render($page, $viewParams);
+(new Controller($request))->run();
